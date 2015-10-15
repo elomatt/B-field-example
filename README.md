c6798558a0ddf0e14eac3361aaba363a96903504
elomatt <matt.aldous@soton.ac.uk>
2015-10-15 15:11:35 +0100

    Update to commented version

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # B-field-example
 Example code showing how to simulate (pair of) solenoid magnetic field.
 
-Code should be self-explanatory, and is written to accompany Stand-Alone experiment B in PHY1017 at University of Southampton.
+Code should be self-explanatory, and is written to accompany Stand-Alone
+experiment B in PHY1017 at University of Southampton.
 
 Any questions should be directed to the author:
 
@@ -17,15 +18,20 @@ Matt Aldous:
     
 Basic usage instructions:
 
-  1) Read and understand the definition of the function "solenoid" (on line 23), with reference to the formulation of the Biot-Savart law in your student text.
+  1) Read and understand the definition of the function "solenoid" (on line 23),
+with reference to the formulation of the Biot-Savart law in your student text
   
-  2) Edit the values of "position_1" and "position_2" (on lines 31 and 32) to reflect the positions of the solenoids relative to a reference point (e.g. for a 20cm separation, set these values to -0.1 and 0.1)
+  2) Edit the values of "position_1" and "position_2" (on lines 46 and 47) to
+reflect the positions of the solenoids relative to a reference point (e.g. for a
+20cm separation, set these values to -0.1 and 0.1)
   
-  3) Set the number of steps along the z-axis at which you want to sample by editing n_steps (line 33)
+  3) Set the number of steps along the z-axis at which you want to sample by
+editing n_steps (line 50)
   
-  4) Edit "id_coils" (line 42) to match the parameters of your coils.
+  4) Edit "id_coils" (line 69) to match the parameters of your coils
   
-  5) Set the units you wish to plot in. Gauss is recommended, but milliTesla and Tesla are other options available
+  5) Set the units you wish to plot in on line 78. Gauss is recommended, but
+milliTesla and Tesla are other options available
   
-  6) Run the script in a python environment and use the output to compare to your characterisation of the coils.
-  
+  6) Run the script in a python environment and use the output to compare to
+your characterisation of the coils
--- bfield.py
+++ bfield.py
@@ -22,38 +22,78 @@ matplotlib.rc('font', **font)
 
 def solenoid(N, I, l, R, z, D=0):
     
+    # Takes parameters:
+    #    N = number of solenoid turns
+    #    I = current in coil
+    #    l = coil depth
+    #    R = coil radius
+    #    z = probe position along axis
+    #    D = coil separation along axis
+
+    # First line calculates a constant premultiplier based on
+    # the Biot-Savart law.    
     k = 4 * np.pi * 1e-7 *N*I/(4*l)
+    
+    # Next two lines calculate the positive and negative components
+    # of z displacement term
     z_plus = (z - D + l)/np.sqrt((z - D + l)**2 + R**2)
     z_minus = (z - D - l)/np.sqrt((z - D - l)**2 + R**2)      
     
+    # Finally, put it all together
     return k*(z_plus - z_minus)
+    
+# Declare initial positions of coils relative to midpoint
+position_1 = -0.1
+position_2 = 0.1
 
-position_1 = -0.07
-position_2 = 0.07
+# Set granularity of simulation (number of probe points on z axis)
 n_steps = 100
-config = "anti-helmholtz"
 
-separations = np.linspace(0.14, 0.5, num=1)
+# Set to either "helmholtz" or "antihelmholtz" depending on desire
+# for either uniform or quadrupole field
+config = "helmholtz"
 
+# Calculates required positions of coils (default is num=1, which will only
+# calculate B field for initial positions)
+separations = np.linspace(position_2 - position_1, 0.5, num=1)
+
+# Set up plotting
 fig = plt.figure()
 ax = fig.add_subplot(111)
 
+# Figure out exactly where the sample points should be
 z_theoretical = np.linspace(position_1, position_2, n_steps + 1)
-id_coils = [100, 2, 0.04, 0.12, z_theoretical]
+
+# This list defines the coil setup and parameters:
+#    [Number of turns, current through each, depth, radius, z probe positions]
+id_coils = [397, 1.5, 0.04, 0.1515, z_theoretical]
+
+# Makes a container for the theoretical values
 b_list_theory = []
-units = "G"
+
+# Set this to the units you want:
+#    "G" = Gauss
+#    "T" = Tesla
+#    "mT" = milliTesla
+units = "mT"
+
+# This line defines the units you refer to above.
 conversion = {"T":1, "mT":1e3, "G":1e4}
 
 ###############################################################################
 #CALCULATIONS
 
+# For a given configuration, set the coil orientation and calculate B at every
+# given z position
 if config == "helmholtz":
     for d in separations:
         b_list_theory.append(solenoid(*id_coils, D=d) + solenoid(*id_coils, D=-d))
+        
 elif config == "anti-helmholtz":
     for d in separations:
         b_list_theory.append(solenoid(*id_coils, D=d) - solenoid(*id_coils, D=-d))
 
+# Apply conversion factor with failsafe to make sure Tesla doesn't convert
 if units in conversion:
     factor = conversion[units]
 else:
@@ -63,12 +103,15 @@ else:
 ###############################################################################
 #DATA OUTPUT
 
+# Create a plot for every separation requested
 for index, item in enumerate(b_list_theory):
     ax.plot(z_theoretical, item*factor, label="%.2fm separation" % separations[index])
     print "|{0:.2f}m separation\t|mean {1:.2f}{2}\t|STD {3:.2f}{2}|".format(
     separations[index], np.mean(item)*factor, units, np.std(item)*factor)
-
+    
+# Make the plots pretty
 ax.set_xlabel("$z$ (m)")
 ax.set_ylabel("$B_z$ (%s)" % units)
 plt.legend(loc="best")
+#plt.axis([position_1, position_2, 0, 0.01])
 plt.show()
