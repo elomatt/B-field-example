098ead32305adf2f6c02fbd0fcf1899e03017ee3
elomatt <matt.aldous@soton.ac.uk>
2016-02-04 20:55:22 +0000

    Updating with new file w/ integral etc

--- bfield.py
+++ bfield.py
@@ -38,49 +38,50 @@ def solenoid(N, I, l, R, z, D=0):
     
     # Next two lines calculate the positive and negative components
     # of z displacement term
-    z_plus = (z - D + l)/np.sqrt((z - D + l)**2 + R**2)
-    z_minus = (z - D - l)/np.sqrt((z - D - l)**2 + R**2)      
+    z_plus = (z - D/2 + l)/np.sqrt((z - D/2 + l)**2 + R**2)
+    z_minus = (z - D/2 - l)/np.sqrt((z - D/2 - l)**2 + R**2)      
     
     # Finally, put it all together
-    return k*(z_plus - z_minus)
+    return k*(-z_plus + z_minus)
     
 # Declare initial positions of coils relative to midpoint
-position_1 = -0.1
-position_2 = 0.1
+position_1 = -0.0125
+position_2 = 0.0125
+d = position_2 - position_1
 
 # Set granularity of simulation (number of probe points on z axis)
-n_steps = 100
+n_steps = 1000
 
 # Set to either "helmholtz" or "antihelmholtz" depending on desire
 # for either uniform or quadrupole field
-config = "helmholtz"
-
-# Calculates required positions of coils (default is num=1, which will only
-# calculate B field for initial positions)
-separations = np.linspace(position_2 - position_1, 0.5, num=1)
+config = "antihelmholtz"
 
 # Set up plotting
 fig = plt.figure()
-ax = fig.add_subplot(111)
+ax_B = fig.add_subplot(111)
+ax_g = ax_B.twinx()
 
 # Figure out exactly where the sample points should be
-z_theoretical = np.linspace(position_1, position_2, n_steps + 1)
+z_theoretical = np.linspace(2*position_1, 2*position_2, n_steps + 1)
 
+z_experimental = (np.array([0,0.06,0.16,0.56,1.06,1.56,2.06,3.06,4.06,5.06,6.06,7.06,8.06])- 4.25)/1000.0
+b_experimental = np.array([293,288,289,281,272,257,246,179,104,21,-51,-87,-94])/1000.0
 # This list defines the coil setup and parameters:
 #    [Number of turns, current through each, depth, radius, z probe positions]
-id_coils = [397, 1.5, 0.04, 0.1515, z_theoretical]
+fyl_coils = [397, 1.5, 0.04, 0.1515, z_theoretical]
+pcb_coils = [5, 0.4, 0.001, 0.006, z_theoretical]
+mini_coils = [15, 0.505, 0.005, 0.0249, z_theoretical]
+id_coils = mini_coils
 
-# Makes a container for the theoretical values
-b_list_theory = []
 
 # Set this to the units you want:
 #    "G" = Gauss
 #    "T" = Tesla
 #    "mT" = milliTesla
-units = "mT"
+units = "G"
 
 # This line defines the units you refer to above.
-conversion = {"T":1, "mT":1e3, "G":1e4}
+conversion = {"T":1, "mT":1e3, "G":1e4, "mG":1e7}
 
 ###############################################################################
 #CALCULATIONS
@@ -88,12 +89,10 @@ conversion = {"T":1, "mT":1e3, "G":1e4}
 # For a given configuration, set the coil orientation and calculate B at every
 # given z position
 if config == "helmholtz":
-    for d in separations:
-        b_list_theory.append(solenoid(*id_coils, D=d) + solenoid(*id_coils, D=-d))
+    b_theory = solenoid(*id_coils, D=d) + solenoid(*id_coils, D=-d)
         
-elif config == "anti-helmholtz":
-    for d in separations:
-        b_list_theory.append(solenoid(*id_coils, D=d) - solenoid(*id_coils, D=-d))
+elif config == "antihelmholtz":
+        b_theory = solenoid(*id_coils, D=d) - solenoid(*id_coils, D=-d)
 
 # Apply conversion factor with failsafe to make sure Tesla doesn't convert
 if units in conversion:
@@ -102,18 +101,21 @@ else:
     units = "T"
     factor = 1
 
+b_gradient= np.diff(b_theory)/np.diff(z_theoretical)
 ###############################################################################
 #DATA OUTPUT
 
-# Create a plot for every separation requested
-for index, item in enumerate(b_list_theory):
-    ax.plot(z_theoretical, item*factor, label="%.2fm separation" % separations[index])
-    print "|{0:.2f}m separation\t|mean {1:.2f}{2}\t|STD {3:.2f}{2}|".format(
-    separations[index], np.mean(item)*factor, units, np.std(item)*factor)
+# Create a plot
+field, = ax_B.plot(z_theoretical, b_theory*factor)
+grad, = ax_g.plot(z_theoretical[1:], b_gradient*factor/100, "--k")
+#expt, = ax_B.plot(z_experimental, b_experimental, ".g")
     
-# Make the plots pretty
-ax.set_xlabel("$z$ (m)")
-ax.set_ylabel("$B_z$ (%s)" % units)
-plt.legend(loc="best")
-#plt.axis([position_1, position_2, 0, 0.01])
+# Make the plot pretty
+ax_B.set_xlabel("$z$ (m)")
+ax_B.set_ylabel("$B_z$ (%s)" % units)
+ax_g.set_ylabel("$\partial_zB$ (%s/cm)" % units)
+fig.legend([field, grad],#, expt],
+           ["B field %.2em separation" % d,
+           "Gradient %.2em separation" % d],#, "Experimental B field data"],
+           loc = "upper center")
 plt.show()
